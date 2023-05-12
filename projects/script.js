6824563d5e946a77916bbfc48768940ba51f2c48
Gaurav Singh Yadav <93148442+GauravYadav15@users.noreply.github.com>
2023-05-12 23:14:37 +0530

    url changed

--- projects/script.js
+++ projects/script.js
@@ -47,10 +47,10 @@ function showProjects(projects) {
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
-        <h3>${project.name}</h3>
+        <h3>${project.name}</h3>z
         </div>
         <div class="desc">
           <p>${project.desc}</p>
