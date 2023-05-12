c8322cd029c7b6933e6f17e5866141e60a033e90
Gaurav Singh Yadav <93148442+GauravYadav15@users.noreply.github.com>
2023-05-12 23:48:09 +0530

    images corrected

--- projects/script.js
+++ projects/script.js
@@ -44,13 +44,14 @@ function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
+    
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="${project.image}" alt="project" />
       <div class="content">
         <div class="tag">
-        <h3>${project.name}</h3>z
+        <h3>Project Name</h3>
         </div>
         <div class="desc">
           <p>${project.desc}</p>
